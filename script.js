bba94ef5ef5efaeef241c90fafb1c4055ef053a7
shaik subhani <118998017+ShaikvSubhani@users.noreply.github.com>
2023-05-15 09:37:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // complete the given function
 
 function palindrome(str){
+let i=0;
+	let j=str.length-1;
+	let isPallindrome=true;
+	while(i<=j)
+		{
+			if(arr[i]!==arr[j])
+			{
+				isPallindrome=false;
+				break;
+			}
 
+			j--;
+			i++;
+		}
+
+	console.log(isPallindrome ? true : false);
 }
 module.exports = palindrome
